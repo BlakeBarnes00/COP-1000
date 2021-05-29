e8b54c9e9a757f53383749e5aac9aaa488d53477
Ellis Barnes <ellisbarnes00@gmail.com>
2021-05-29 14:08:16 -0400

    Saved file?

---
+++ Chapter 4 - Repetition Structures/Assignments/4.4 - Calculating a Running Total/51189.py
@@ -0,0 +1,5 @@
+# Write a statement that increments total by the value associated with the
+# amount. add value associated with amount to that associated with total 
+# and assign the result to total
+
+total += amount
