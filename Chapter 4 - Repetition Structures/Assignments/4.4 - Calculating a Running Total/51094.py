0b7cd3c1604104efbb1fbd6a78b40bf0ccce4f3a
Ellis Barnes <ellisbarnes00@gmail.com>
2021-05-29 09:44:14 -0400

    Completed 4.4 Calculating running total assignments

---
+++ Chapter 4 - Repetition Structures/Assignments/4.4 - Calculating a Running Total/51094.py
@@ -0,0 +1,2 @@
+# increase variable bridge_players by 4
+bridge_players += 4
---
+++ Chapter 4 - Repetition Structures/Assignments/4.4 - Calculating a Running Total/51095.py
@@ -0,0 +1,4 @@
+# given the variable profits, write a statment that
+# increases its value by a factor of 10
+
+profits *= 10
